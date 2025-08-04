9cd84a1ebf9a376ae1dfbdd53b6ac7cc7386ec4b
Y.Zakharchenko <Y.Zakharchenko@steel.mil.ua>
2025-08-04 23:03:25 +0300

    ruff fix

--- itvdn_django/settings/test.py
+++ itvdn_django/settings/test.py
@@ -1,8 +1,8 @@
 """
 Test settings for ITVDN Django Study Project
 """
-import environ
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
