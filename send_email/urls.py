266e099ddd86c5eaa15591b86a647c3e662f233e
Y.Zakharchenko <Y.Zakharchenko@steel.mil.ua>
2025-09-11 15:34:12 +0300

    user creation form refactored; dependencies added; mailchimp subscr init

--- send_email/urls.py
+++ send_email/urls.py
@@ -1,13 +1,16 @@
 from django.contrib.auth import views as auth
+from django.shortcuts import redirect
 from django.urls import path
 
-from .views import ImageEmailTemplateView, SimpleEmailTemplateView, activate_account, usersignup
+from .views import ImageEmailTemplateView, SimpleEmailTemplateView, activate_account, usersignup, subscription
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/email-signup/', permanent=False), name='home'),
     path("simple-email/", SimpleEmailTemplateView.as_view(), name="email-test"),
     path("image-email/", ImageEmailTemplateView.as_view(), name="image-email"),
     path("logout/", auth.LogoutView.as_view(template_name="index.html"), name="logout"),
-    path(r"signup/", usersignup, name="register_user"),
+    path("email-signup/", usersignup, name="email-signup"),
     path(r"activate/<uidb64>/<token>/", activate_account, name="activate"),
-    # path("subscription/", subscription, name="subscription"),
+    path("subscription/", subscription, name="subscription"),
 ]
